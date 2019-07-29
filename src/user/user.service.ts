3c84b6a69840150437f53a43fa5a60fd8dead039
em2046 <em2046@qq.com>
2019-07-29 23:03:40 +0800

    password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,18 @@
+const crypto = require('crypto');
+
+function pbkdf2Promise(password: string) {
+  return new Promise(resolve => {
+    crypto.pbkdf2(password, 'salt', 100000, 64, 'sha512', (err, derivedKey) => {
+      if (err) {
+        throw err;
+      }
+      const encrypted: string = derivedKey.toString('hex');
+
+      resolve(encrypted);
+    });
+  });
+}
+
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,6 +27,13 @@ export class UserService {
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    return await this.userRepository.save(createUserDto);
+    const user = new User();
+    user.name = createUserDto.name;
+    user.email = createUserDto.email;
+    user.avatar = createUserDto.avatar;
+
+    user.password = await pbkdf2Promise(createUserDto.password);
+
+    return await this.userRepository.save(user);
   }
 }
